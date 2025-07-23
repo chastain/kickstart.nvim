616cb1cf2082c0391c8a078d38de76a5c52c8381
chastain <eric@chastain.me>
2025-07-23 16:25:28 -0400

    update theme to kakanagawa

--- init.lua
+++ init.lua
@@ -876,26 +876,63 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      --@diagnostic disable-next-line: missing-fields
+      require('kanagawa').setup {
+
+        -- Default options:
+        require('kanagawa').setup {
+          compile = false, -- enable compiling the colorscheme
+          undercurl = true, -- enable undercurls
+          commentStyle = { italic = true },
+          functionStyle = {},
+          keywordStyle = { italic = true },
+          statementStyle = { bold = true },
+          typeStyle = {},
+          transparent = false, -- do not set background color
+          dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+          terminalColors = true, -- define vim.g.terminal_color_{0,17}
+          colors = { -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+          },
+          overrides = function(colors) -- add/modify highlights
+            return {}
+          end,
+          theme = 'wave', -- Load "wave" theme
+          background = { -- map the value of 'background' option to a theme
+            dark = 'dragon', -- 'wave', -- try "dragon" !
+            light = 'lotus',
+          },
+        },
+      }
+
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+    --'folke/tokyonight.nvim',
+    --priority = 1000, -- Make sure to load this before all the other start plugins.
+    --config = function()
+    --  ---@diagnostic disable-next-line: missing-fields
+    --  require('tokyonight').setup {
+    --    styles = {
+    --      comments = { italic = false }, -- Disable italics in comments
+    --    },
+    --  }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
+    --  -- Load the colorscheme here.
+    --  -- Like many other themes, this one has different styles, and you could load
+    --  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --  vim.cmd.colorscheme 'tokyonight-moon'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
